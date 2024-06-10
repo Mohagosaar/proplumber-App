7f3daf9ada4a9fef1895e53e0c5eb74ee926f1be
Moha Ali <Moha.gosaar@gmail.com>
2024-06-09 19:27:27 -0500

    Vocation planner app

--- vacationplanner/client/src/components/ListHeader/ListHeader.jsx
+++ vacationplanner/client/src/components/ListHeader/ListHeader.jsx
@@ -1,14 +1,17 @@
+import Modul from "../Modules/Modal";
+
 const ListHeader = ({ listNames }) => {
   const signOut = () => {};
   return (
     <div className="list-hdeader">
       <h1>{listNames}</h1>
       <div className="btn-container">
-        <button className="btn-create">ADD NEW TASK</button>
-        <button className="sign-out" onClick={signOut}>
+        <button className="btn-addTask">ADD NEW TASK</button>
+        <button className="btn-sigtout" onClick={signOut}>
           signout
         </button>
       </div>
+      <Modul />
     </div>
   );
 };
--- vacationplanner/client/src/components/Modules/Modal.jsx
+++ vacationplanner/client/src/components/Modules/Modal.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
 const Modul = () => {
+  const mode = "create";
+  const handleForm = (event) => {};
   return (
-    <div>
-      <h1>Mohamed Ali</h1>
+    <div className="overley">
+      <div className="modul">
+        <div className="from-groups">
+          <h3>Let's {mode} your task</h3>
+          <button>X</button>
+        </div>
+        <form>
+          <input
+            required
+            maxLength={50}
+            placeholder=" Add your Task"
+            name="title"
+            value={""}
+            onChange={handleForm}
+          />
+          <label for=" range"> Select your current progress</label>
+          <input
+            required
+            id="range"
+            type="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={""}
+            onChange={handleForm}
+          />
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 };
